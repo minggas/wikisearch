ea2d9d1e725d65597e779356d0cfd30dd729c93a
minggas <fasfora@gmail.com>
2018-03-26 00:46:31 -0300

    Made CSS and fix some JS

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
-$(document).ready(function() {
-    const $items = $("#items");
-    const $inputField = $("#input");
-    const $searchBtn = $("#searchButton");
-  
-    //Call the Wiki API, get results and render on the page
-    function getResult() {
-      let url = `https://en.wikipedia.org//w/api.php?action=query&format=json&list=search&continue=&srsearch=${$inputField.val()}&srwhat=text&callback=?`;
-      $.getJSON(url, response => {
-        response.query.search.forEach(item => {
-          $items.append(
-            `<a target="_blank" href="http://en.wikipedia.org/?curid=${
-              item.pageid
-            }"><h3>${item.title}</h3></a><p>${item.snippet}</p><hr>`
-          );
-        });
+$(document).ready(function () {
+  const $items = $("#items");
+  const $inputField = $("#input");
+  const $searchBtn = $("#searchButton");
+
+  //Call the Wiki API, get results and render on the page
+  function getResult() {
+    let url = `https://en.wikipedia.org//w/api.php?action=query&format=json&list=search&continue=&srsearch=${$inputField.val()}&srwhat=text&callback=?`;
+    $.getJSON(url, response => {
+      response.query.search.forEach(item => {
+        $items.append(
+          `<div class="item">
+            <a target="_blank" href="http://en.wikipedia.org/?curid=${item.pageid}">
+              <h4>${item.title}</h4>
+              <br>
+              ${item.snippet}
+            </a>
+          </div>`
+        );
       });
-    }
-  
-    //Function to call the results of the search
-    function showResult() {
-      getResult();
-      return false;
-    }
-    //Call function on submit
-    $searchBtn.click(showResult);
-  });
-  
+    });
+  }
+
+  //Function to call the results of the search
+  function showResult() {
+    getResult();
+    return false;
+  }
+
+  //Call function on submit
+  $searchBtn.click(showResult);
+
+});
